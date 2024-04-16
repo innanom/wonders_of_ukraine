eda8614167cee06c07c9df6b79ca85eef091d164
Inna Nomerovkina <inkosof@gmail.com>
2024-04-16 16:37:17 +0300

    gallery

--- js/gallery-slider.js
+++ js/gallery-slider.js
@@ -5,9 +5,10 @@ const swiper = new Swiper('.swiper', {
   loop: true,
 
   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
   // Navigation arrows
   navigation: {
